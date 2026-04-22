ee216bc6abd82ab25a8b27d3f2d9593419a69658
Gascarpa <gascarparo02@gmail.com>
2026-04-22 01:21:14 -0300

    Implementado update

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,7 +34,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new ID = "+ newSeller.getId());
 		
+		System.out.println("\n=== Test 4: Seller update===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 		}
-		
 	
 	}
